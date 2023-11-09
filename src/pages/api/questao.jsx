9bc0c65f3fbb54c782df94ab0d0d1b9edf76c116
Mayara <mayara.b.carvalho@aluno.senai.br>
2023-11-09 20:56:39 -0300

    #aula09-11

--- src/pages/api/questao.jsx
+++ src/pages/api/questao.jsx
@@ -1,12 +1,31 @@
-export default function questao(){
-    fetch ('http://localhost: 4077/api/questao/1020') //Testar a chamada a API - Isso vai retornar uma promessa e como resposta vai ter um método json que vai trazer a resposta.
-    //Ele também vai retornar uma promessa.
-    .then(resp => resp.json() ) //Aqui vai chegar a resposta que contém um método Json
-    .then(resp => console.log(json)) // Aqui vai pegar o Json recebido do Back-end e mostrar no console.
+import { useEffect, useState } from "react";
 
-    return(
+const LoadingSpinner = () => {
+    return <div>Carregando...</div>;
+};
+
+export default function questao() {
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [questao, setQuestao] = useState({});
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        fetch("http://localhost: 4077/api/questao/8045")
+          .then(resp => resp.json())
+          .then(questao => setQuestao(questao));
+    }, []);
+
+    if (!questao) {
+        return <LoadingSpinner />;
+    }
+
+    return (
         <div>
-            Questão
+            <h1>Questão</h1>
+            <div>
+                <span>{questao.enunciado}</span>
+            </div>
         </div>
     )
 }
